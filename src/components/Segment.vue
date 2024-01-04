f2bf688987d24ba11c2cad4259997e5d4f4db485
baran <baran@fledra.dev>
2024-01-04 07:05:17 +0300

    add segment display for hour and minute

---
+++ src/components/Segment.vue
@@ -0,0 +1,39 @@
+<template>
+  <svg :width="radius * 18" :height="radius * 24">
+    <SegmentLine :size="radius" :active="segments.a" direction="horizontal" :x="15" :y="5" />
+    <SegmentLine :size="radius" :active="segments.b" direction="vertical" :x="87.5" :y="2" />
+    <SegmentLine :size="radius" :active="segments.c" direction="vertical" :x="82" :y="47" />
+    <SegmentLine :size="radius" :active="segments.d" direction="horizontal" :x="5" :y="95" />
+    <SegmentLine :size="radius" :active="segments.e" direction="vertical" :x="14" :y="47" />
+    <SegmentLine :size="radius" :active="segments.f" direction="vertical" :x="20" :y="2" />
+    <SegmentLine :size="radius" :active="segments.g" direction="horizontal" :x="9" :y="50" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+import type { Digit } from '@/types';
+
+const props = defineProps<{ radius: number; value: Digit }>();
+const { value, radius } = toRefs(props);
+
+const segmentMap = {
+  0: { a: true, b: true, c: true, d: true, e: true, f: true, g: false },
+  1: { a: false, b: true, c: true, d: false, e: false, f: false, g: false },
+  2: { a: true, b: true, c: false, d: true, e: true, f: false, g: true },
+  3: { a: true, b: true, c: true, d: true, e: false, f: false, g: true },
+  4: { a: false, b: true, c: true, d: false, e: false, f: true, g: true },
+  5: { a: true, b: false, c: true, d: true, e: false, f: true, g: true },
+  6: { a: true, b: false, c: true, d: true, e: true, f: true, g: true },
+  7: { a: true, b: true, c: true, d: false, e: false, f: false, g: false },
+  8: { a: true, b: true, c: true, d: true, e: true, f: true, g: true },
+  9: { a: true, b: true, c: true, d: true, e: false, f: true, g: true },
+};
+
+const segments = computed(() => segmentMap[value.value]);
+</script>
+
+<style lang="scss" scoped>
+circle {
+  fill: color(primary);
+}
+</style>
---
+++ src/components/SegmentLine.vue
@@ -0,0 +1,37 @@
+<template>
+  <g v-if="props.direction === 'horizontal'">
+    <circle v-for="i in 4" :key="i" :cx="`${props.x + i * 15}%`" :cy="`${y}%`" :r="props.size" :class="{ active }" />
+  </g>
+
+  <g v-else>
+    <circle
+      v-for="i in 4"
+      :key="i"
+      :cx="`${props.x - i * 1.5}%`"
+      :cy="`${props.y + i * 10}%`"
+      :r="props.size"
+      :class="{ active }"
+    />
+  </g>
+</template>
+
+<script setup lang="ts">
+interface SegmentLineProps {
+  size: number;
+  x: number;
+  y: number;
+  direction: 'vertical' | 'horizontal';
+  active: boolean;
+}
+const props = defineProps<SegmentLineProps>();
+</script>
+
+<style lang="scss" scoped>
+circle {
+  fill: rgba(color(primary), 0.1);
+
+  &.active {
+    fill: color(primary);
+  }
+}
+</style>
